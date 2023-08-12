82768ef1f2c3a01684c030c6ce0b5403dd4f19b2
Berke Turanlioglu <berke.turanlioglu@gmail.com>
2023-08-12 19:53:43 +0300

    Initial Commit

---
+++ TipKitProject/TipKitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipKitProjectApp.swift
+//  TipKitProject
+//
+//  Created by Berke Turanlıoğlu on 12.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TipKitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
